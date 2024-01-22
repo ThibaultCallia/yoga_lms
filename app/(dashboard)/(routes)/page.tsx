85744262f9dade4725cbdc501c29dd1585ab283f
Thibault Calliauw <calliauw.t@gmail.com>
2024-01-22 13:36:10 +1100

    Clerk set up

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl ">test</p>
-      <Button>Test</Button>
+      <Button>test</Button>
     </div>
   );
 }
